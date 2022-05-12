e39b786b795d6aaadbffe46327f54c19e73969ca
jolly318 <105202898+jolly318@users.noreply.github.com>
2022-05-12 09:31:59 +0800

    Add files via upload

---
+++ tcgaEstimate.symbol2id.R
@@ -0,0 +1,25 @@
+###Video source: http://study.163.com/provider/1026136977/index.htm?share=2&shareId=1026136977
+######Video source: http://www.biowolf.cn/shop/
+######生信自学网: http://www.biowolf.cn/
+######合作邮箱：2749657388@qq.com
+######答疑微信: 18520221056
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("org.Hs.eg.db", version = "3.8")
+
+
+setwd("C:\\Users\\lexb4\\Desktop\\tcgaEstimate\\17.symbol2id")
+
+library("org.Hs.eg.db")
+rt=read.table("intersectGenes.txt",sep="\t",check.names=F,header=T)
+genes=as.vector(rt[,1])
+entrezIDs <- mget(genes, org.Hs.egSYMBOL2EG, ifnotfound=NA)
+entrezIDs <- as.character(entrezIDs)
+out=cbind(rt,entrezID=entrezIDs)
+write.table(out,file="id.txt",sep="\t",quote=F,row.names=F)
+
+###Video source: http://study.163.com/provider/1026136977/index.htm?share=2&shareId=1026136977
+######Video source: http://www.biowolf.cn/shop/
+######生信自学网: http://www.biowolf.cn/
+######合作邮箱：2749657388@qq.com
+######答疑微信: 18520221056
